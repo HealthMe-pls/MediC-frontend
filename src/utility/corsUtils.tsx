5febff9950192319afaabcec712ab28a1369101b
FAH <apichaya.inkhiewsai@gmail.com>
2025-01-23 02:40:08 +0700

    fix : cors header

---
+++ src/utility/corsUtils.tsx
@@ -0,0 +1,8 @@
+export function setCorsHeaders(headers: Headers) {
+  headers.set("Access-Control-Allow-Origin", "*");
+  headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+}
