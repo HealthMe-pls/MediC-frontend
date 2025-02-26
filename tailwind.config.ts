7d67720acab0a3ee5b22e87ba2b8eb01196d21ee
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-02-27 00:51:57 +0700

    add : screens size to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,14 @@ export default {
         24: "24px",
         40: "40px",
       },
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   variants: {
