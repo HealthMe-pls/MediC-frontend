32efcc67c20d987788c9561a5b0d98df19fa0e37
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-02-27 17:59:56 +0700

    add : about us page

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import Header from "../layouts/Header";
+
+export default function AboutUsPage() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>About Us Page should be here</h1>
+      </div>
+    </>
+  );
+}
