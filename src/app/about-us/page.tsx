12e91a78e147bbb8c2951c0cd587369331790ff9
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-02-27 18:03:33 +0700

    fix : font lexend on banner not working

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,11 +2,11 @@ import Header from "../layouts/Header";
 
 export default function AboutUsPage() {
   return (
-    <>
+    <div className="font-lexend">
       <Header />
       <div>
         <h1>About Us Page should be here</h1>
       </div>
-    </>
+    </div>
   );
 }
