c25422d07fe7ee6844d2a8e450991393cac04249
Issawara <kmind46@gmail.com>
2025-03-05 19:27:45 +0700

    EditAccountInformation

---
+++ src/app/vendor/edit-account-information/page.tsx
@@ -0,0 +1,11 @@
+import VendorLayouts from "@/app/layouts/VendorLayouts";
+
+export default function EditAccountInformation() {
+  return (
+    <VendorLayouts currentPage="Edit Account Information">
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1>Welcome to Bamboo Family Market Backdoors!!!</h1>
+      </div>
+    </VendorLayouts>
+  );
+}
