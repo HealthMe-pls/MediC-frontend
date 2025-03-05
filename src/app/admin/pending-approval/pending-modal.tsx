8084adfb40bb88d0efe2268f60b429a27744f63b
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-03-05 18:52:35 +0700

    fix : editTIme color at modal

--- src/app/admin/pending-approval/pending-modal.tsx
+++ src/app/admin/pending-approval/pending-modal.tsx
@@ -108,7 +108,7 @@ export default function PendingModal({ onClose, tempshop }: PendingModalProps) {
                       {`${formatDate(date.start_time)} `}
                       <span
                         className={`${
-                          date.type === "add" ? "text-[#52A794]" : ""
+                          date.type === "edit" ? "text-[#52A794]" : ""
                         }`}
                       >
                         {`${formatTime(date.start_time)} - ${formatTime(
@@ -119,31 +119,6 @@ export default function PendingModal({ onClose, tempshop }: PendingModalProps) {
                   )
                 )}
               </ul>
-              <p>Updated Opening Schedule :</p>
-              <div className="flex flex-row">
-                <ul>
-                  {Array.isArray(shop.shop_open_dates) &&
-                    shop.shop_open_dates.map(
-                      (date: ShopOpenDates, index: number) => (
-                        <li key={index} className="text-[14px] font-light">
-                          {`${formatDate(date.start_time)} ${formatTime(
-                            date.start_time
-                          )} - ${formatTime(date.end_time)}`}
-                        </li>
-                      )
-                    )}
-                </ul>
-                <ul>
-                  {Array.isArray(tempshop.time) &&
-                    tempshop.time.map((date: ShopOpenDates, index: number) => (
-                      <li key={index} className="text-[14px] font-light">
-                        {`${formatDate(date.start_time)} ${formatTime(
-                          date.start_time
-                        )} - ${formatTime(date.end_time)}`}
-                      </li>
-                    ))}
-                </ul>
-              </div>
             </div>
             <p className="font-normal">Menu:</p>
             <div className="px-4">
